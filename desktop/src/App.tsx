3f8cb033ba9c23dff22cc60b3f8a9dbab3b5a2d0
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-09-13 21:31:59 -0300

    landing page finalizada

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,8 +1,9 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
 import logoImg from "./assets/nlw-logo.svg";
 
 const App = () => {
   return (
-    <div className="max-w[1344px] mx-auto flex items-center flex-col my-20">
+    <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
       <img src={logoImg} alt="logo" />
 
       <h1 className="text-6xl text-white font-black mt-20">
@@ -70,7 +71,23 @@ const App = () => {
         </a>
       </div>
 
-      <div className="bg-[#2A2624] px-8 py-6"></div>
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8  overflow-hidden">
+        <div className="bg-[#2A2624] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition-colors flex gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
